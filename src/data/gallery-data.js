2bedd96a092f781947b883678bee22bc955ed2ad
KEVIN ANDRES ALFONSO GAVIRIA <115943086+darkshadow208@users.noreply.github.com>
2026-02-06 01:06:32 -0500

    segundo

---
+++ src/data/gallery-data.js
@@ -0,0 +1,31 @@
+
+import img1 from '../assets/images/001_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img2 from '../assets/images/002_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img3 from '../assets/images/003_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img4 from '../assets/images/004_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img5 from '../assets/images/005_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img6 from '../assets/images/006_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img7 from '../assets/images/007_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img8 from '../assets/images/008_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img9 from '../assets/images/009_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img10 from '../assets/images/010_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img11 from '../assets/images/011_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img12 from '../assets/images/012_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img13 from '../assets/images/013_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img14 from '../assets/images/014_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img15 from '../assets/images/015_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img16 from '../assets/images/016_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img17 from '../assets/images/017_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img18 from '../assets/images/018_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img19 from '../assets/images/019_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img20 from '../assets/images/020_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img21 from '../assets/images/021_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img22 from '../assets/images/022_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img23 from '../assets/images/023_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+import img24 from '../assets/images/024_tu_san_agustin_15_anos_mariam_forest_lugares_para_eventos_historias.webp';
+
+export const mariamEvent = [
+    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
+    img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
+    img21, img22, img23, img24
+];
